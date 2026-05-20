072f3cfc38e7425c507ec47e6ad00808247c7e56
Thøger Bro <tbro1995@gmail.com>
2026-05-20 13:02:15 +0200

    sandwich

--- code/blocking_button.cpp
+++ code/blocking_button.cpp
@@ -1,6 +1,8 @@
 #include "blocking_button.h"
 #include <Arduino.h>
 
+// Button we can add, s.t. we can block progress in the solver until we press this button (for manual intervention)
+// Maybe we should also add an led that indicates we are waiting for button press?
 const int buttonPin = 1;
 
 void initButton() {
